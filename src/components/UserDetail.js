c3d769e1f50ac76e11d3b4fa9530dfd8a126f8de
rahuljindal18 <rahuljindal1989@gmail.com>
2020-04-20 13:08:46 +0530

    default text added

--- src/components/UserDetail.js
+++ src/components/UserDetail.js
@@ -2,29 +2,36 @@ import React from "react";
 import { connect } from "react-redux";
 
 function UserDetail({ user }) {
+  const { username } = user;
   return (
-    <div className="container">
-      <h1>User Details</h1>
-      <div className="field-div-detail">
-        <label>Username : </label>
-        <h3>{user.username}</h3>
-      </div>
-      <div className="field-div-detail">
-        <label>Email : </label>
-        <h3>{user.email}</h3>
-      </div>
-      <div className="field-div-detail">
-        <label>Image : </label>
-        <img src={user.imageFile} alt="" height="200" width="200" />
-      </div>
-      <div className="field-div-detail">
-        <label>Address : </label>
-        <h3>{user.address}</h3>
-      </div>
-      <div className="field-div-detail">
-        <label>Hobbies : </label>
-        <h3>{user.hobbies.map((obj) => obj.name).join(",")}</h3>
-      </div>
+    <div>
+      {username ? (
+        <div className="container">
+          <h1>User Details</h1>
+          <div className="field-div-detail">
+            <label>Username : </label>
+            <h3>{user.username}</h3>
+          </div>
+          <div className="field-div-detail">
+            <label>Email : </label>
+            <h3>{user.email}</h3>
+          </div>
+          <div className="field-div-detail">
+            <label>Image : </label>
+            <img src={user.imageFile} alt="" height="200" width="200" />
+          </div>
+          <div className="field-div-detail">
+            <label>Address : </label>
+            <h3>{user.address}</h3>
+          </div>
+          <div className="field-div-detail">
+            <label>Hobbies : </label>
+            <h3>{user.hobbies.map((obj) => obj.name).join(",")}</h3>
+          </div>
+        </div>
+      ) : (
+        <h3 style={{ textAlign: "center" }}>No detail to display</h3>
+      )}
     </div>
   );
 }
